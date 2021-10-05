2dda356c9c8d925f5cdee91c9995f42b693ac71a
jahid <info.jahidulislamjahid@gmail.com>
2021-10-05 20:28:40 +0600

    farther polishing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,99 +1,83 @@
 import React, { useEffect, useState } from 'react';
-// import { Carousel } from 'react-bootstrap';
+import { Button, Carousel } from 'react-bootstrap';
 import './Home.css';
-import Courses from '../Courses/Courses';
 
 
 const Home = () => {
-    const [primeMinisters, setPrimeMinister] = useState([]);
+    const [courses, setCourse] = useState([]);
 
     useEffect(() => {
-        fetch('./minister.JSON')
+        fetch("./custom-data.json")
             .then(res => res.json())
-            .then(data => setPrimeMinister(data))
-    }, [])
-
-    // Handle invitation
-    const [invitation, setInvitation] = useState([]);
-    const handleInvitation = (primeMinister) => {
-        const newInvitation = [...invitation, primeMinister];
-        setInvitation(newInvitation);
-    }
-
-
+            .then(data => setCourse(data))
+    }, []);
     return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
 
-                    </div>
-                    {/* All Prime Ministers */}
-                    <div className="col-md-3">
-                        {/* Single Prime Minister */}
-                        <div className="pt-0 ps-2">
-                            {
-                                primeMinisters.map(primeMinister => <Courses
-                                    key={primeMinister.id}
-                                    primeMinister={primeMinister}
-                                ></Courses>
-                                )
-                            }
-                        </div>
-                    </div>
-                    {/* <div className="col-md-3">
-                    { Invitation Cart  }
-                    <Cart invitation={invitation}></Cart>   
-                </div>               */}
+        <div>
+            <div className="home-section">
+                <div className="conatiner">
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://hbu-files.wpmucdn.com/uploads/sites/83/2021/05/Great-Texts-major.jpg"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h2 className="">Enroll Today On Our Academic Programs.</h2>
+                                <p>Learn From Our Special And Award Winning Teachers Around The World. </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://www.sfu.ca/content/sfu/grc/research/publications/2020/jcr:content/main_content/textimage/image.img.640.medium.jpg/1593050509560.jpg"
+                                alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h2 className="">Enroll Today On Our Computer Programs.</h2>
+                                <p className="">Learn From Our Special And Award Winning Teachers Around The World. </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://i2.wp.com/www.bookstoprisoners.net/wp-content/uploads/2018/02/science-fiction.png?fit=1200%2C400&ssl=1"
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h2 className="">Enroll Today On Our Language Programs.</h2>
+                                <p className="">Learn From Our Special And Award Winning Teachers Around The World. </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
+
+            </div>
+            <h1 className="my-5 fw-bold text-primary">Our Popular Courses</h1>
+            <div className="row">
+                {
+                    courses.map((course) => (<div className="col-md-4">
+                        <div className="card-style">
+                            <div className="logo-image">
+                                <img className="w-25 my-3" src={course.img} alt="" />
+                            </div>
+                            <h2> <strong>Course: </strong>{course.course}</h2>
+                            <h3 className="fw-bold">Price : ${course.price}</h3>
+                            <h5>Contact : {course.Contact}</h5>
+                            <Button className="btn-warning px-5"> <strong>Enroll</strong> </Button>
+                        </div>
+                    </div>))
+                }
             </div>
 
         </div>
     );
 };
 // return (
-//     <div className="home-section">
-//         <div className="conatiner">
-//             <Carousel>
-//                 <Carousel.Item>
-//                     <img
-//                         className="d-block w-100"
-//                         src="https://hbu-files.wpmucdn.com/uploads/sites/83/2021/05/Great-Texts-major.jpg"
-//                         alt="First slide"
-//                     />
-//                     <Carousel.Caption>
-//                         <h2 className="">Enroll Today On Our Academic Programs.</h2>
-//                         <p>Learn From Our Special And Award Winning Teachers Around The World. </p>
-//                     </Carousel.Caption>
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                     <img
-//                         className="d-block w-100"
-//                         src="https://www.sfu.ca/content/sfu/grc/research/publications/2020/jcr:content/main_content/textimage/image.img.640.medium.jpg/1593050509560.jpg"
-//                         alt="Second slide"
-//                     />
 
-//                     <Carousel.Caption>
-//                         <h2 className="">Enroll Today On Our Computer Programs.</h2>
-//                         <p className="">Learn From Our Special And Award Winning Teachers Around The World. </p>
-//                     </Carousel.Caption>
-//                 </Carousel.Item>
-//                 <Carousel.Item>
-//                     <img
-//                         className="d-block w-100"
-//                         src="https://i2.wp.com/www.bookstoprisoners.net/wp-content/uploads/2018/02/science-fiction.png?fit=1200%2C400&ssl=1"
-//                         alt="Third slide"
-//                     />
-
-//                     <Carousel.Caption>
-//                         <h2 className="">Enroll Today On Our Language Programs.</h2>
-//                         <p className="">Learn From Our Special And Award Winning Teachers Around The World. </p>
-//                     </Carousel.Caption>
-//                 </Carousel.Item>
-//             </Carousel>
-//         </div>
-
-//     </div>
 
 // );
 // };
--- src/Components/Notfound/Notfound.js
+++ src/Components/Notfound/Notfound.js
@@ -8,7 +8,8 @@ const Notfound = () => {
     }
     return (
         <div className="notfound-section">
-            <h2 className="text-danger fw-bold mt-5 pt-5"> Sorry! The Page You Are Looking For Cannot Be Reached.</h2>
+            <h4 className="text-danger fw-bold mt-5 pt-5"> Sorry! The Page You Are Looking For Cannot Be Reached.</h4>
+            <img src="https://i.redd.it/l051i0y03e761.png" alt="" className="w-100" />
             <Button onClick={goBack}>Go Back</Button>
         </div>
     );
